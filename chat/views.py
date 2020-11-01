73807115be9adeaae822842d5b050f99533b2aac
bluewaree <belkihelmehdi2@gmail.com>
2020-11-01 17:34:34 +0100

    feat: add username input layout

--- chat/views.py
+++ chat/views.py
@@ -3,4 +3,14 @@ from django.conf import settings
 
 
 def chat_view(request):
-    return render(request, "chat.html", {"ws_url": settings.WESBSOCKET_URL})
+    if request.method == "POST":
+        if "username" in request.POST:
+            username = request.POST["username"]
+            request.session["username"] = username
+            return render(request, "chat.html", {"ws_url": settings.WESBSOCKET_URL, "username": username})
+
+    if request.session.get("username", None):
+        return render(request, "chat.html", {"ws_url": settings.WESBSOCKET_URL, "username": username})
+    else:
+        return render(request, "input.html")
+
