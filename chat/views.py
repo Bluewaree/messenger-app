3ea647a7bd16d54d7f0dc30d0692196c4a9f55ae
bluewaree <belkihelmehdi2@gmail.com>
2020-11-01 18:02:52 +0100

    feat: add chat messages page

--- chat/views.py
+++ chat/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render
 from django.conf import settings
 
+from chat.models import Message
+
 
 def chat_view(request):
     if request.method == "POST":
         if "username" in request.POST:
             username = request.POST["username"]
             request.session["username"] = username
-            return render(request, "chat.html", {"ws_url": settings.WESBSOCKET_URL, "username": username})
+            messages = Message.objects.all()
+            return render(
+                request,
+                "chat.html",
+                {
+                    "ws_url": settings.WESBSOCKET_URL,
+                    "username": username,
+                    "messages": messages,
+                }
+            )
 
     if request.session.get("username", None):
         username = request.session["username"]
-        return render(request, "chat.html", {"ws_url": settings.WESBSOCKET_URL, "username": username})
+        messages = Message.objects.all()
+        return render(
+            request,
+            "chat.html",
+            {
+                "ws_url": settings.WESBSOCKET_URL,
+                "username": username,
+                "messages": messages,
+            }
+        )
     else:
         return render(request, "input.html")
 
---
+++ static/js/index.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('.messages-container').scrollTop($('.messages-container')[0].scrollHeight);
+});
