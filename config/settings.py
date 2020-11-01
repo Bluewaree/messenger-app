87c35aabb6448048db54b7718d7d28cccad3b7c3
bluewaree <belkihelmehdi2@gmail.com>
2020-11-01 21:54:42 +0100

    feat: add postgres setup

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
+from configurations.values import Value
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,12 +80,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASE_URL = Value(
+    "postgres://postgres:password@db:5432/chat",
+    environ_name="DATABASE_URL",
+    environ_prefix="",
+)
+DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
+# "default": dj_database_url.parse(DATABASE_URL),
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
 # Password validation
