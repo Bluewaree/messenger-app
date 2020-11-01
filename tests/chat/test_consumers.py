12eb553ed683ced6a12c959520e5eae611087648
bluewaree <belkihelmehdi2@gmail.com>
2020-11-01 16:25:00 +0100

    feat: test message instances storage

--- tests/chat/test_consumers.py
+++ tests/chat/test_consumers.py
@@ -5,6 +5,8 @@ import asyncio
 from config.routing import application
 from channels.layers import get_channel_layer
 
+from chat.models import Message
+
 TEST_CHANNEL_LAYERS = {
     "default": {"BACKEND": "channels.layers.InMemoryChannelLayer",},
 }
@@ -21,16 +23,31 @@ class TestChatConsumer:
         assert connected is True
         return communicator
 
+    @database_sync_to_async
+    def check_message_objects_count(self, count):
+        assert Message.objects.count() == count
+
+    @database_sync_to_async
+    def check_message_object_fields(self):
+        message = Message.objects.last()
+        assert message.message == self.message
+        assert message.message == self.username
+
     @pytest.mark.django_db
     @pytest.mark.asyncio
     async def test_chat_process(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
-        channel_layer = get_channel_layer()
+        channel_layer = get_channel_layer(0)
 
+        messages = await self.check_message_objects_count(0)
         self.communicators = []
         self.communicators.append(await self.auth_connect("communicator1", "chat/"))
         self.communicators.append(await self.auth_connect("communicator2", "chat/"))
-        await self.communicators[0].send_json_to({"message": "Hello", "username": "communicator1"})
+        self.message = "Hello"
+        self.username = "communicator1"
+        await self.communicators[0].send_json_to({"message": self.message, "username": self.username})
+        messages = await self.check_message_objects_count(1)
+        messages = await self.check_message_object_fields()
 
         for communicator in self.communicators:
             response = await communicator.receive_json_from()
